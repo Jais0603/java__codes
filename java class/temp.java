85eb8044ef0f4ec71d127e3980dc9921889ac132
Jais0603 <98961661+Jais0603@users.noreply.github.com>
2023-04-08 22:51:51 +0530

    Add files via upload

---
+++ java class/temp.java
@@ -0,0 +1,7 @@
+public class temp{
+
+    public static void main(String args[])
+     {
+            System.out.println("Welcome to first class of java of 4th sem");
+     }
+}
---
+++ java class/whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int u =8;
+        do{
+
+            System.out.println("Hey J");
+            u=u+2;
+        }
+        while (u<20);
+        
+        
+    }
+}
